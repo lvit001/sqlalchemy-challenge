d713b8cf23cd3ac1691fdf49567394446a8eff14
lvit001 <140283164+lvit001@users.noreply.github.com>
2023-12-07 21:25:16 -0600

    finished the precipitation link in the api file

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -30,30 +30,30 @@ session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
-# app = Flask(__name__)
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
-# @app.route("/")
-# def home():
-#     return (
-#         f"Welcome to the Climate API!<br/>"
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/precipitation<br/>"
-#         f"/api/v1.0/stations<br/>"
-#         f"/api/v1.0/tobs<br/>"
-#         f"/api/v1.0/<start><br/>"
-#         f"/api/v1.0/<start>/<end><br/>"
-#     )
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the Climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
 
-# @app.route("/api/v1.0/precipitation")
-# def precipitation():
-#     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-#     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
-#     precipitation_rows = [{"Date": result[0], "Precipitation": result[1]} for result in results]
-#     return jsonify(precipitation_rows)
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
+    precipitation_rows = [{"Date": result[0], "Precipitation": result[1]} for result in results]
+    return jsonify(precipitation_rows)
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
