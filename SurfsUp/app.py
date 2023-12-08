6197f8c0883a0917b35559bbcef82aa51847cc1c
lvit001 <140283164+lvit001@users.noreply.github.com>
2023-12-08 16:30:57 -0600

    created code for the tobs and start api routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -35,6 +35,7 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
 @app.route("/")
 def home():
     # home page and routes
@@ -44,15 +45,23 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/[start_date_here]<br/>"
+        f"Please format start date as follows: yyyy-mm-dd<br/>"
+        f"/api/v1.0/[start_date_here]/[end_date_here]<br/>"
+        f"Please format start and end dates as follows: yyyy-mm-dd"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # provide the precipitation data for the previous year
+
+    # find one year before the most recent date of data
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # query the data for date and precipitation for all dates after the year ago date determined above
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
+    # close the session
+    session.close()
+    # list comprehension to get a list of dictionaries including the date and precipitation
     precipitation_rows = [{"Date": result[0], "Precipitation": result[1]} for result in results]
     return jsonify(precipitation_rows)
 
@@ -60,10 +69,48 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     # return a list of the stations
+
+    # query for the stations
     stations = session.query(Station.station).all()
-    station_rows = [{"Station": station[0]} for station in stations]
+    # close the session
+    session.close()
+    # list comprehension to get a list of the stations
+    station_rows = [station[0] for station in stations]
     return jsonify(station_rows)
 
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Return a JSON list of temperature observations for the previous year.
+
+    # find one year before the most recent date of data
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # query the data for date and temperature for all dates after the year ago date determined above and for a specific station
+    temp_data = session.query(Measurement.date,Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= year_ago).all()
+    # close the session
+    session.close()
+    # list comprehension to get a list of dictionaries including the date and temperature
+    tobs = [{"Date": result[0], "Temperature": result[1]} for result in temp_data]
+    return jsonify(tobs)
+
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    # For a specified start, calculate TMIN, TAVG, and TMAX for 
+    # all the dates greater than or equal to the start date.
+
+    # create a variable to contain the data we want to select
+    sel = [Measurement.date,
+    func.min(Measurement.tobs),
+    func.max(Measurement.tobs),
+    func.avg(Measurement.tobs)]
+    #query for the TMIN, TMAX, and TAVG for the request date and all after
+    temp_query = session.query(*sel).filter(Measurement.date >= start).all()
+    # close the session
+    session.close()
+    # list comprehension to get a list of dictionaries including the date, TMIN, TMAX, and TAVG
+    temp_list = [{"Date": result[0], "TMIN": result[1], "TMAX": result[2], "TAVG": result[3]} for result in temp_query]
+    return jsonify(temp_list)
 
 
 if __name__ == "__main__":
