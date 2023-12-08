7b83616ad42b3e6561c9b67a3636442a276b8955
lvit001 <140283164+lvit001@users.noreply.github.com>
2023-12-07 21:21:59 -0600

    began code for api app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,59 @@
 # Import the dependencies.
+import datetime as dt
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+# app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+# @app.route("/")
+# def home():
+#     return (
+#         f"Welcome to the Climate API!<br/>"
+#         f"Available Routes:<br/>"
+#         f"/api/v1.0/precipitation<br/>"
+#         f"/api/v1.0/stations<br/>"
+#         f"/api/v1.0/tobs<br/>"
+#         f"/api/v1.0/<start><br/>"
+#         f"/api/v1.0/<start>/<end><br/>"
+#     )
+
+# @app.route("/api/v1.0/precipitation")
+# def precipitation():
+#     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+#     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
+#     precipitation_rows = [{"Date": result[0], "Precipitation": result[1]} for result in results]
+#     return jsonify(precipitation_rows)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
